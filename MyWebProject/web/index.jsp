78e101b7642b7b12ea386269e514a55df2e42331
tom_glb <goodMorning_glb@mlf.com>
2022-06-19 11:22:57 +0800

    my first commit

---
+++ MyWebProject/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrator
+  Date: 2022/6/18
+  Time: 13:03
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>尚硅谷官网</title>
+  </head>
+  <body>
+    <h1 style="color:orangered">你好！尚硅谷！</h1>
+  </body>
+</html>
---
+++ src/com/mlf/java/HelloWorld.java
@@ -0,0 +1,21 @@
+package com.mlf.java;
+
+import java.util.ArrayList;
+//F4:当前类的树型结构
+public class HelloWorld {
+    
+
+    public static void main(String[] args){
+
+        System.out.println("hello world");
+        System.out.println("hello world");
+        ArrayList<Object> objects = new ArrayList<>();
+
+
+        String s = new String("张三");
+        int length = s.length();
+        System.out.println(length);
+    }
+
+
+}
